5332b646c4ad004c33e5222c8dfcf283abcf4ac5
mikolaszko <mikosodz.lote@gmail.com>
2024-01-08 19:20:08 +0100

    fucking dune command is EXEC not RUN why does it even run??? it doesnt exist

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,23 @@
-type database = 
-  {dbd : Sqlite3.db; created : (string, unit) Hashtbl.t}
-let () = print_endline (Sqlite3.sqlite_version_info ());
+open Sqlite3
+open Printf
 
+let () = print_endline "Hello World"
+let assert_ok rc = assert (rc = Rc.OK)
+let db = db_open "t_exec";;
+
+for i = 0 to 10 do
+  print_int 5;
+  try
+    let drop = sprintf "DROP TABLE IF EXISTS tbl%d" i
+    and sql =
+      sprintf "CREATE TABLE tbl%d (a varchar(1), b INTEGER, c FLOAT)" i
+    in
+    printf "%d %s\n%!" i sql;
+    assert_ok (exec db drop);
+    match exec db sql ~cb:(fun _ _ -> print_endline "???") with
+    | Rc.OK -> ()
+    | _ ->
+        printf "Failed: %s\n" (errmsg db);
+        assert false
+  with xcp -> print_endline (Printexc.to_string xcp)
+done
